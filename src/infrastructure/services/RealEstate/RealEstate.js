b5edbee1d03b9ac0847a534f33b0900ec7aa23a7
deal <95deal@gmail.com>
2020-11-24 10:39:21 -0300

    created RealEstate queries

---
+++ src/infrastructure/services/RealEstate/RealEstate.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const { base_url, token: apiToken } = require('../../http/api')
+
+module.exports = class RealEstate {
+  _httpClient
+
+  constructor(_httpClient) {
+    this._httpClient = _httpClient
+  }
+
+  async getRealEstate() {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/real_state`, { params: { token: apiToken } });
+    return data;
+  }
+
+  async queryRealEstate(id) {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/real_state/${id}`, { params: { token: apiToken } });
+    return data;
+  }
+
+}
