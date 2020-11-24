ac9164fd0e4f832e65c7a7c380154e0ff69be637
deal <95deal@gmail.com>
2020-11-24 10:16:07 -0300

    created Residential methods

---
+++ src/infrastructure/services/Residential/Residential.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { base_url, token: apiToken } = require('../../http/api')
+
+module.exports = class Residential {
+  _httpClient
+
+  constructor(_httpClient) {
+    this._httpClient = _httpClient
+  }
+
+  async getResidentials() {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/residentials`, { params: { token: apiToken } });
+    return data
+  }
+
+  async getResidentialsByCPF(cpfDto) {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/residentials`, { params: { token: apiToken, cpf: cpfDto } });
+    return data;
+  }
+
+  async getResidentialsByCNPJ(cnpjDto) {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/residentials`, { params: { token: apiToken, cnpj: cnpjDto } });
+    return data;
+  }
+
+  async queryResidential(id) {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/residentials/${id}`, { params: { token: apiToken } });
+    return data;
+  }
+
+}
