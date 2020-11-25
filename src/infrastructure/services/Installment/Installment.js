72d5fd0f86e5dd5f776d714ddd256bfd41679af0
deal <95deal@gmail.com>
2020-11-25 13:13:30 -0300

    created Installment class

---
+++ src/infrastructure/services/Installment/Installment.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { base_url, token: apiToken, config } = require('../../http/api')
+
+module.exports = class Installment {
+  _httpClient
+
+  constructor(_httpClient) {
+    this._httpClient = _httpClient
+  }
+
+  async queryInstallments(start, end) {
+    const { data } = await this._httpClient.get(`${base_url}/api/v1/installments`,
+      {
+        params:
+          { token: apiToken, start_at: start, end_at: end }
+      }, config)
+    return data;
+  }
+}
